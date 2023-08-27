d2d287542d0fd042b2b99f4674cb95c69bc35966
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-08-27 10:15:49 +0000

    Deployed contract on depolia network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const SEPOLIA_URl = process.env.SEPOLIA_URl;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.17",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_URl,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
